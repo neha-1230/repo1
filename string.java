4dea5757a8f5344181c12fd170d7ed5277ea2f93
neha <nehashingade66@gmail.com>
2024-06-25 12:14:15 +0530

    Third commit done

---
+++ string.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Test 
+{
+	public static void main(String args[])
+	{
+		string name;
+		Scanner ab=new Scanner(System.in);
+		System.out.println("Enter a Name");
+		name=ab.nextLine();
+		System.out.println(name);
+	}
+}
